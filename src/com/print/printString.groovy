8aa9ccdaf4cd51736aeb2e6de7e9d75920bf5f44
yogesh9391 <yogeshvj.jilhawar@gmail.com>
2021-03-26 14:56:00 +0530

    Update printString.groovy

--- src/com/print/printString.groovy
+++ src/com/print/printString.groovy
@@ -2,9 +2,11 @@ package com.print
 public class printString {
   def printHello() {
      println "Hello"
+    return true
   }
 
   def printQn() {
      println "How are you ?"
+    return true
   }
 }
